0b80ee13fd7ecfe2b3a1d515cbe8461805fa9970
Jason Phung <jphun020@uottawa.ca>
2024-10-20 23:57:43 -0400

    Initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyA2I14fioAlVGarnzvVIMC7HcifMgwZ6R0",
+        authDomain: "addressbook-47be5.firebaseapp.com",
+        projectId: "addressbook-47be5",
+        storageBucket: "addressbook-47be5.appspot.com",
+        messagingSenderId: "1033344019105",
+        appId: "1:1033344019105:web:ab84a682d758d9bed79ee0",
+        measurementId: "G-5KDE9XG4Z1"
+    }
+};
